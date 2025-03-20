ac560af25b7079e1c1d7105cb716cdef16c80b73
Sreeman Reddy <139124397+sreemanrp@users.noreply.github.com>
2025-03-20 23:36:24 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const staticAssets = [
+    './',
+    './index.html',
+    // Add other assets like CSS, JS if you have separate files
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open('static-assets');
+    cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req));
+    }
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('dynamic-assets');
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        return await cache.match(req);
+    }
+}
